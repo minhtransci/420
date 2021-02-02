adcac473b848c92cdeea8a42a1796f6a00418566
minhtransci <42896553+minhtransci@users.noreply.github.com>
2021-02-01 20:59:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -107,104 +107,32 @@ def get_options(list_stocks):
 # Initialize the app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, FONT_AWESOME])
 server = app.server
-app.layout = html.Div(
-    children=[
-        html.Div(className='row',
-                 children=[
-                    html.Div(className='four columns div-user-controls',
-                             children=[
-                                 html.H2('Dash - STOCK PRICES'),
-                                 html.P('''Visualising time series with Plotly - Dash'''),
-                                 html.P('''Pick one or more stocks from the dropdown below.'''),
-                                 html.Div(className='div-for-dropdown',
-                                          children=[
-                                              dcc.Dropdown(id='stockselector',
-                                                           options=get_options(df['stock'].unique()),
-                                                           multi=True,
-                                                           value=[df['stock'].sort_values()[0]],
-                                                           style={'backgroundColor': '#1E1E1E'},
-                                                           className='stockselector')
-                                          ],
-                                          style={'color': '#1E1E1E'})
-                             ]
-                             ),
-                    html.Div(className='eight columns div-for-charts bg-grey',
-                             children=[
-                                 dcc.Graph(id='timeseries',
-                                           config={'displayModeBar': False},
-                                           animate=True
-                                           ),
-                                dcc.Graph(id='change', config={'displayModeBar': False}, animate=True)
-                             ])
-                              ])
+
+
+card_content = [
+    dbc.CardHeader("Card header"),
+    dbc.CardBody(
+        [
+            html.H5("Card title", className="card-title"),
+            html.P(
+                "This is some card content that we'll reuse",
+                className="card-text",
+            ),
         ]
-)
-@app.callback(Output('timeseries', 'figure'),
-              [Input('stockselector', 'value')])
-def update_timeseries(selected_dropdown_value):
-    ''' Draw traces of the feature 'value' based one the currently selected stocks '''
-    # STEP 1
-    trace = []
-    df_sub = df
-    # STEP 2
-    # Draw and append traces for each stock
-    for stock in selected_dropdown_value:
-        trace.append(go.Scatter(x=df_sub[df_sub['stock'] == stock].index,
-                                 y=df_sub[df_sub['stock'] == stock]['value'],
-                                 mode='lines',
-                                 opacity=0.7,
-                                 name=stock,
-                                 textposition='bottom center'))
-    # STEP 3
-    traces = [trace]
-    data = [val for sublist in traces for val in sublist]
-    # Define Figure
-    # STEP 4
-    figure = {'data': data,
-              'layout': go.Layout(
-                  colorway=["#5E0DAC", '#FF4F00', '#375CB1', '#FF7400', '#FFF400', '#FF0056'],
-                  template='plotly_dark',
-                  paper_bgcolor='rgba(0, 0, 0, 0)',
-                  plot_bgcolor='rgba(0, 0, 0, 0)',
-                  margin={'b': 15},
-                  hovermode='x',
-                  autosize=True,
-                  title={'text': 'Stock Prices', 'font': {'color': 'white'}, 'x': 0.5},
-                  xaxis={'range': [df_sub.index.min(), df_sub.index.max()]},
-              ),
-              }
-    return figure
-@app.callback(Output('change', 'figure'),
-              [Input('stockselector', 'value')])
-def update_change(selected_dropdown_value):
-    ''' Draw traces of the feature 'change' based one the currently selected stocks '''
-    trace = []
-    df_sub = df
-    # Draw and append traces for each stock
-    for stock in selected_dropdown_value:
-        trace.append(go.Scatter(x=df_sub[df_sub['stock'] == stock].index,
-                                 y=df_sub[df_sub['stock'] == stock]['change'],
-                                 mode='lines',
-                                 opacity=0.7,
-                                 name=stock,
-                                 textposition='bottom center'))
-    traces = [trace]
-    data = [val for sublist in traces for val in sublist]
-    # Define Figure
-    figure = {'data': data,
-              'layout': go.Layout(
-                  colorway=["#5E0DAC", '#FF4F00', '#375CB1', '#FF7400', '#FFF400', '#FF0056'],
-                  template='plotly_dark',
-                  paper_bgcolor='rgba(0, 0, 0, 0)',
-                  plot_bgcolor='rgba(0, 0, 0, 0)',
-                  margin={'t': 50},
-                  height=250,
-                  hovermode='x',
-                  autosize=True,
-                  title={'text': 'Daily Change', 'font': {'color': 'white'}, 'x': 0.5},
-                  xaxis={'showticklabels': False, 'range': [df_sub.index.min(), df_sub.index.max()]},
-              ),
-              }
-    return figure
+    ),
+]
+
+app.layout = html.Div([
+    dcc.Tabs(id="tabs-styled-with-inline", value='tab-1', children=[
+        dcc.Tab(label='Tab 1', value='tab-1'),
+        dcc.Tab(label='Tab 2', value='tab-2'),
+        dcc.Tab(label='Tab 3', value='tab-3'),
+        dcc.Tab(label='Tab 4', value='tab-4'),
+        dcc.Tab(label='Tab 5', value='tab-5'),
+    ], ),
+    html.Div(id='tabs-content-inline')
+])
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)

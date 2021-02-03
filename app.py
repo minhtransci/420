a6fcbd52dfbca75daed466f5dfddd3e400569fb4
minhtransci <42896553+minhtransci@users.noreply.github.com>
2021-02-02 22:15:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -376,23 +376,6 @@ def MultiStepPlot(state):
                       config={'displayModeBar': False},
                       figure=fig11
                       ),
-            dcc.Graph(id='timeseries',
-                      config={'displayModeBar': False},
-                      figure=fig12
-                      ),
-
-            dcc.Graph(id='timeseries',
-                      config={'displayModeBar': False},
-                      figure=fig13
-                      ),
-            dcc.Graph(id='timeseries',
-                      config={'displayModeBar': False},
-                      figure=fig14
-                      ),
-            dcc.Graph(id='timeseries',
-                      config={'displayModeBar': False},
-                      figure=fig15
-                      ),
 
     ]
     )
